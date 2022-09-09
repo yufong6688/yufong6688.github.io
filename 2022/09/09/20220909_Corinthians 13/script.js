509abb2abde4e5bf872f4c0c4d6ca4db0ad6419c
yufong6688 <cyc0815@gmail.com>
2022-09-09 21:41:24 +0800

    Site updated: 2022-09-09 21:41:24

---
+++ 2022/09/09/20220909_Corinthians 13/script.js
@@ -0,0 +1,32 @@
+$(function () {
+    var curAudio;
+    $(".audio-button").click(function (e) {
+        e.preventDefault();
+        var song = $(this).next('audio')[0];
+
+        if (curAudio && song != curAudio && !curAudio.paused) {
+            curAudio.pause();
+            $(curAudio).prev().find('.btn-text').text('Listen');
+            $(curAudio).prev().find('.btn-icon').html('');
+        }
+
+        if (song.paused) {
+            song.play();
+            curAudio = song;
+            $(this).find('.btn-text').text('Stop');
+            $(this).find('.btn-icon').html('');
+        } else {
+            song.pause();
+            curAudio = undefined;
+            $(this).find('.btn-text').text('Listen');
+            $(this).find('.btn-icon').html('');
+        }
+        curPlaying = $(this).parent()[0].id;
+    });
+
+    $('audio').on('ended', function() {
+       $(this).prev().find('.btn-text').text('Listen');
+       $(this).prev().find('.btn-icon').html('');
+    });  
+  
+});
